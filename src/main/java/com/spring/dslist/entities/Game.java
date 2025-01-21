096c54b3ce63d405403b2d46c677af8908da46c2
Elivelton Souza <67668057+EliveltonSouzaDev@users.noreply.github.com>
2025-01-20 22:31:11 -0300

    game seed

--- src/main/java/com/spring/dslist/entities/Game.java
+++ src/main/java/com/spring/dslist/entities/Game.java
@@ -12,27 +12,34 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
     @Column(name = "game_year")
     private Integer year;
-    private String  genre;
-    private String platform;
-    private  String imgUrl;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
-    private String longDescriptions;
 
-    public Game(){
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
 
+    public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String platform, String genre, String imgUrl, String shortDescription, String longDescriptions) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+                String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
-        this.platform = platform;
         this.genre = genre;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
-        this.longDescriptions = longDescriptions;
+        this.longDescription = longDescription;
     }
 
     public Long getId() {
@@ -67,12 +74,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
@@ -91,12 +106,12 @@ public class Game {
         this.shortDescription = shortDescription;
     }
 
-    public String getLongDescriptions() {
-        return longDescriptions;
+    public String getLongDescription() {
+        return longDescription;
     }
 
-    public void setLongDescriptions(String longDescriptions) {
-        this.longDescriptions = longDescriptions;
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
     }
 
     @Override
